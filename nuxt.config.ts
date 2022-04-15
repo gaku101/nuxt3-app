4338da36eab298f857d8618d3219ec066a4839af
takehiro <takehiro-r133@hotmail.co.jp>
2022-04-16 01:35:42 +0900

    delete microcms related codes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,4 @@
 import { defineNuxtConfig } from "nuxt3"
-const { BASE_URL, API_KEY } = process.env
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
-export default defineNuxtConfig({
-  publicRuntimeConfig: {
-    baseURL: BASE_URL,
-    apiKey: API_KEY,
-  },
-})
+export default defineNuxtConfig({})
--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,9 @@
 <script setup>
-const ctx = useRuntimeConfig();
-
-const { data } = await useFetch("/blog", {
-  baseURL: ctx.baseURL,
-  headers: {
-    "X-MICROCMS-API-KEY": ctx.apiKey,
-  }
-});
 </script>
 
 
 <template>
   <div>
     <h1>Hello, Nuxt3!</h1>
-    <ul>
-      <li v-for="article in data.contents" :key="article.id">
-        <nuxt-link :to="`/${article.id}`">{{ article.title }}</nuxt-link>
-        <!-- <p>{{ article.title }}</p> -->
-      </li>
-    </ul>
   </div>
 </template>
